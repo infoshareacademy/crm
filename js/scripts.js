7a5543e69710013d34e785db33b5b7c73cc7e268
krasai <krasuskip@gmail.com>
2015-10-08 16:25:27 +0200

    Revert commita - Struktura HTML dla projektu CRM

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-        $(this).find(".fa-bars").toggleClass("fa-rotate-90");
-    });
-});
