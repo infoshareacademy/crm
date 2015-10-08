732e87fbe95becc81a258ab060749d8c1a60f679
krasai <krasuskip@gmail.com>
2015-10-08 15:42:52 +0200

    Struktura HTML dla projektu CRM

---
+++ includes/content.php
@@ -0,0 +1,30 @@
+<main id="page-content-wrapper">
+    <header>
+        <ul class="nav navbar-right">
+            <li><i class="fa fa-user fa-fw"></i> Marta Kowalska</li>
+        </ul>
+    </header>
+
+    <!-- Tab panes -->
+    <div class="tab-content">
+
+        <!-- Home Page Content -->
+        <?php include 'pages/home.php'; ?>
+
+        <!-- Lista Kontrahentów Page Content -->
+        <?php include 'pages/lista-kontrahentow.php'; ?>
+
+        <!-- Dodaj kontrahenta Page Content -->
+        <?php include 'pages/dodaj-kontrahenta.php'; ?>
+
+        <!-- Lista spotkań Page Content -->
+        <?php include 'pages/lista-spotkan.php'; ?>
+
+        <!-- Zaplanuj spotkanie Page Content -->
+        <?php include 'pages/zaplanuj-spotkanie.php'; ?>
+
+        <!-- Lista spotkań Page Content -->
+        <?php include 'pages/generuj-raport.php'; ?>
+
+    </div>
+</main>
