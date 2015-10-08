4f74951476c6e0e07b48314878794450313dda32
katban <grancen@wp.pl>
2015-10-08 17:01:09 +0200

    display all data from clients (automatically)

--- show_clients.php
+++ show_clients.php
@@ -17,48 +17,24 @@ $dbh = new PDO ("mysql:host=$hostname;port=3307;dbname=$db",$username,$pass);
 $dbh->query("SET NAMES utf8");
 $dbh->query("SET CHARACTER_SET utf8_polish_ci");
 
+//struktura tabeli
+// idClient   |  nameClient   |  idTax  |  addressClient  |  cityClient  |  phoneClient  |  faxClient  |  wwwClient  |  mailClient  |  noteClient  | creationDateClient
 $sql = "SELECT * FROM clients";
 
 $clients = $dbh->query($sql);
 
 echo '<meta charset="utf8">';
 
+
 echo '<table><tbody>';
 foreach ($clients as $client) {
     echo '<tr>';
+    $iloscElementow = count($client);
+    for ($i=0; $i < $iloscElementow; $i++) {
         echo '<td>';
-            print $client['idClient'];
+            echo $client[$i];
         echo '</td>';
-        echo '<td>';
-            print $client['nameClient'];
-        echo '</td>';
-        echo '<td>';
-            print $client['idTax'];
-        echo '</td>';
-        echo '<td>';
-            print $client['cityClient'];
-        echo '</td>';
-        echo '<td>';
-            print $client['addressClient'];
-        echo '</td>';
-        echo '<td>';
-            print $client['phoneClient'];
-        echo '</td>';
-        echo '<td>';
-            print $client['faxClient'];
-        echo '</td>';
-        echo '<td>';
-            print $client['wwwClient'];
-        echo '</td>';
-        echo '<td>';
-            print $client['mailClient'];
-        echo '</td>';
-        echo '<td>';
-            print $client['noteClient'];
-        echo '</td>';
-    //.' - '.$client['nameClient'].' - '.$client['idTax'].' - '.$client['cityClient'].' - '.'<br />';
-    // idClient   |  nameClient   |  idTax  |  addressClient  |  cityClient  |  phoneClient  |  faxClient  |  wwwClient  |  mailClient  |  noteClient  | creationDateClient
-
+    }
     echo '</tr>';
 }
 echo '</tbody></table>';
