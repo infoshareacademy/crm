ca0dea2805bddfaaee2c4a1bd494da63d7206aad
katban <grancen@wp.pl>
2015-10-09 15:35:19 +0200

    CRM-15: connecting to db and displays content from 'clients' table

--- show_clients.php
+++ show_clients.php
@@ -6,37 +6,26 @@
  * Time: 15:25
  */
 
-$hostname = "sql.infoshareaca.nazwa.pl";
-$db = "infoshareaca_5";
-$username = "infoshareaca_5";
-$pass = "F0r3v3r!";
-
-
-
-$dbh = new PDO ("mysql:host=$hostname;port=3307;dbname=$db",$username,$pass);
-$dbh->query("SET NAMES utf8");
-//$dbh->query("SET CHARACTER_SET utf8_polish_ci");
+include('dbconnect.php');
 
 //struktura tabeli
 // idClient   |  nameClient   |  idTax  |  addressClient  |  cityClient  |  phoneClient  |  faxClient  |  wwwClient  |  mailClient  |  noteClient  | creationDateClient
 $sql = "SELECT * FROM clients";
-// tablica zawierajaca wszystkie rekordy z tabeli clients
 $clients = $dbh->query($sql);
 
 echo '<meta charset="utf8">';
 
-
 echo '<table>';
 // naglowek
 echo '<thead>
-            <tr><td>[ ID ]</td><td>[ Company ]</td><td>[ Tax ID ]</td><td>[ Adress ]</td><td>[ City ]</td><td>[ Phone ]</td><td>[ Fax ]</td><td>[ WWW ]</td><td>[ @ ]</td><td>[ Note ]</td>d><td>[ Date ]</td></tr>
+            <tr><td>[ ID ]</td><td>[ Company ]</td><td>[ Tax ID ]</td><td>[ Adress ]</td><td>[ City ]</td><td>[ Phone ]</td><td>[ Fax ]</td><td>[ WWW ]</td><td>[ @ ]</td><td>[ Note ]</td><td>[ Date ]</td></tr>
         </thead>';
 
 echo '<tbody>';
 // wyswietlanie wynikow
 foreach ($clients as $client) {
     echo '<tr>';
-    $iloscElementow = count($client);
+    $iloscElementow = count($client)/2;
     for ($i=0; $i < $iloscElementow; $i++) {
         echo '<td>';
             echo $client[$i];
