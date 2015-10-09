bc514d6c9dfd47e5d2c451b1919be5071e7a99f9
katban <grancen@wp.pl>
2015-10-09 09:07:37 +0200

    CRM-15: add navigation bar in table (problem with show a result)

--- show_clients.php
+++ show_clients.php
@@ -15,18 +15,25 @@ $pass = "F0r3v3r!";
 
 $dbh = new PDO ("mysql:host=$hostname;port=3307;dbname=$db",$username,$pass);
 $dbh->query("SET NAMES utf8");
-$dbh->query("SET CHARACTER_SET utf8_polish_ci");
+//$dbh->query("SET CHARACTER_SET utf8_polish_ci");
 
 //struktura tabeli
 // idClient   |  nameClient   |  idTax  |  addressClient  |  cityClient  |  phoneClient  |  faxClient  |  wwwClient  |  mailClient  |  noteClient  | creationDateClient
 $sql = "SELECT * FROM clients";
-
+// tablica zawierajaca wszystkie rekordy z tabeli clients
 $clients = $dbh->query($sql);
 
 echo '<meta charset="utf8">';
 
 
-echo '<table><tbody>';
+echo '<table>';
+// naglowek
+echo '<thead>
+            <tr><td>[ ID ]</td><td>[ Company ]</td><td>[ Tax ID ]</td><td>[ Adress ]</td><td>[ City ]</td><td>[ Phone ]</td><td>[ Fax ]</td><td>[ WWW ]</td><td>[ @ ]</td><td>[ Note ]</td>d><td>[ Date ]</td></tr>
+        </thead>';
+
+echo '<tbody>';
+// wyswietlanie wynikow
 foreach ($clients as $client) {
     echo '<tr>';
     $iloscElementow = count($client);
