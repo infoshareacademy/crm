f67fb46fbd6c961318a0d16a84f4dac722103d22
fiona <anna.mikulinska@gmail.com>
2015-10-14 16:35:54 +0200

    new format for Event class - first step for update

---
+++ Event.class.php
@@ -0,0 +1,139 @@
+<?php
+
+class Event {
+
+    protected $topicOfEvent;
+    protected $idClient;
+    protected $dateOfEvent;
+    protected $timeOfEvent;
+    protected $statusOfEvent;
+    protected $descriptionOfEvent;
+    protected $typeOfEvent;
+    protected $idContact;
+    protected $idOfEvent;
+    protected $outcomeOfEvent;
+
+    const EVENT_ARRANGED = 01;
+    const EVENT_CONFIRMED = 02;
+    const EVENT_COMPLETED = 03;
+    const EVENT_CANCELLED = 04;
+
+    const OUTCOME_SUCCESS = 01;
+    const OUTCOME_FAILURE = 02;
+    const OUTCOME_FOLLOWUP = 03;
+
+    const MAX_DESCRIPTION_LENGTH = 250;
+    const MAX_TOPIC_LENGTH = 50;
+
+    public function __construct() {
+
+
+    }
+
+    public function __set($param_name, $param_value) {
+        $this->$param_name = $param_value;
+//        switch ($param_name) {
+//            case 'topicOfEvent':
+//                if (!$param_value || strlen($param_value)>self::MAX_TOPIC_LENGTH)
+//                    $this->topicOfEvent = null;
+//                else
+//                    $this->topicOfEvent = htmlspecialchars($param_value);
+//                break;
+//
+//            case 'idClient':
+//                if (!$param_value)
+//                    $this->idClient = null;
+//                else
+//                    $this->idClient = $param_value;
+//                break;
+//
+//            case 'dateOfEvent':
+//                if (!$param_value)
+//                    $this->dateOfEvent = null;
+//                else
+//                    $this->dateOfEvent = $param_value;
+//                break;
+//
+//            case 'timeOfEvent':
+//                if (!$param_value)
+//                    $this->timeOfEvent = null;
+//                else
+//                    $this->timeOfEvent = $param_value;
+//                break;
+//
+//            case 'statusOfEvent':
+//                if (!$param_value)
+//                    $this->statusOfEvent = null;
+//                else
+//                    $this->statusOfEvent = $param_value;
+//                break;
+//
+//            case 'typeOfEvent':
+//                if (!$param_value)
+//                    $this->typeOfEvent = null;
+//                else
+//                    $this->typeOfEvent = $param_value;
+//                break;
+//
+//            case 'descriptionOfEvent':
+//                if (!$param_value || strlen($param_value)>self::MAX_DESCRIPTION_LENGTH)
+//                    $this->descriptionOfEvent = null;
+//                else
+//                    $this->descriptionOfEvent = htmlspecialchars($param_value);
+//                break;
+//            default:
+//                throw new Exception ('There is no such field!');
+//                break;
+//        }
+    }
+
+    public function __get($param_name) {
+        return $this->$param_name;
+    }
+
+
+    public function _sendToDB(){
+        try {
+            $pdo = new PDO('mysql:dbname=infoshareaca_5;host=sql.infoshareaca.nazwa.pl', 'infoshareaca_5', 'F0r3v3r!');
+            $stmt = $pdo->prepare("INSERT INTO events (idClient,
+                                                       dateEvent,
+                                                       timeEvent,
+                                                       typeEvent,
+                                                       statusEvent,
+                                                       descriptionEvent,
+                                                       topicEvent) VALUES (
+                                                                      :idClient,
+                                                                      :dateOfEvent,
+                                                                      :timeOfEvent,
+                                                                      :typeOfEvent,
+                                                                      :statusOfEvent,
+                                                                      :descriptionOfEvent,
+                                                                      :topicOfEvent
+                             );"
+            );
+
+            $stmt->execute(array(
+                    ':idClient' => $this->idClient,
+                    ':dateOfEvent' => $this->dateOfEvent,
+                    ':timeOfEvent' => $this->timeOfEvent,
+                    ':typeOfEvent' => $this->typeOfEvent,
+                    ':statusOfEvent' => $this->statusOfEvent,
+                    ':descriptionOfEvent' => $this->descriptionOfEvent,
+                    ':topicOfEvent' => $this->topicOfEvent
+                )
+            );
+            print_r($stmt);
+
+//            $stmt->debugDumpParams();
+//
+//            $stmt2 = $pdo->prepare("SELECT * FROM events");
+//            $stmt2->execute();
+//            echo count($stmt2->fetchAll());
+        } catch(Exception $e) {
+            print_r($e->getMessage());
+        }
+    }
+
+
+
+}
--- eventClass.php
+++ eventClass.php
@@ -1,151 +1,73 @@
 <?php
+require_once 'Event.class.php';
 
-class Event {
-
-// basic attributes of newly created Event:
-
-    public $topicOfEvent;
-    public $idClient;
-    public $dateOfEvent;
-    public $timeOfEvent;
-    public $statusOfEvent;
-    public $descriptionOfEvent;
-    public $typeOfEvent;
-
-// additional (not required) attributes of newly created Event:
-
-    protected $idContact; // if the counterpart's representative is indicated
-
-
-// other attributes added to the Event when updated/closed:
-
-    protected $idOfEvent; // will be set based on return from DB //
-    protected $outcomeOfEvent; // Event is closed - the result for reporting purpose
-
-// constant values for the methods of this class in the human friendly notation
-
-    const STATUS_ARRANGED = 01;
-    const STATUS_CONFIRMED = 02;
-    const STATUS_COMPLETED = 03;
-    const STATUS_CANCELLED = 04;
-
-    const OUTCOME_SUCCESS = 01;
-    const OUTCOME_FAILURE = 02;
-    const OUTCOME_FOLLOWUP = 03;
-
-// methods of Event class:
-
-    public function __construct($topicOfEvent,
-                                $idClient,
-                                $dateOfEvent,
-                                $timeOfEvent,
-                                $statusOfEvent,
-                                $typeOfEvent,
-                                $descriptionOfEvent) {
-        $this->topicOfEvent = $topicOfEvent;
-        $this->idClient = $idClient;
-        $this->dateOfEvent = $dateOfEvent;
-        $this->timeOfEvent = $timeOfEvent;
-        $this->statusOfEvent = $statusOfEvent;
-        $this->typeOfEvent = $typeOfEvent;
-        $this->descriptionOfEvent = $descriptionOfEvent;
-
-    }
-
-    // function collecting data from $_POST submitted via the Event Form ??
-//
-//    protected function _getDataForNewEvent(){
-//
-//    }
-
-    // function connecting to the DB and inserting the values from Object
-
-    public function _sendEventDataToDB(){
-        try {
-            $pdo = new PDO('mysql:dbname=infoshareaca_5;host=sql.infoshareaca.nazwa.pl', 'infoshareaca_5', 'F0r3v3r!');
-            $stmt = $pdo->prepare("INSERT INTO events (idClient,
-                                                       dateEvent,
-                                                       timeEvent,
-                                                       typeEvent,
-                                                       statusEvent,
-                                                       descriptionEvent,
-                                                       topicEvent) VALUES (
-                                                                      :idClient,
-                                                                      :dateOfEvent,
-                                                                      :timeOfEvent,
-                                                                      :typeOfEvent,
-                                                                      :statusOfEvent,
-                                                                      :descriptionOfEvent,
-                                                                      :topicOfEvent
-                             );"
-            );
-
-            $stmt->execute(array(
-                    ':idClient' => $this->idClient,
-                    ':dateOfEvent' => $this->dateOfEvent,
-                    ':timeOfEvent' => $this->timeOfEvent,
-                    ':typeOfEvent' => $this->typeOfEvent,
-                    ':statusOfEvent' => $this->statusOfEvent,
-                    ':descriptionOfEvent' => $this->descriptionOfEvent,
-                    ':topicOfEvent' => $this->topicOfEvent
-                )
-            );
-            print_r($stmt);
-
-//            $stmt->debugDumpParams();
-//
-//            $stmt2 = $pdo->prepare("SELECT * FROM events");
-//            $stmt2->execute();
-//            echo count($stmt2->fetchAll());
-        } catch(Exception $e) {
-            print_r($e->getMessage());
-        }
-    }
-
-
-
-}
 
 // save input from the form in the variable if some info is missing when form is submitted
 
+$error = array();
 if (count($_POST)) {
-    $topicOfEvent = $_POST['topicOfEvent'];
-    $idClient = $_POST['idClient'];
-    $dateOfEvent = $_POST['dateOfEvent'];
-    $timeOfEvent = $_POST['timeOfEvent'];
-    $statusOfEvent = $_POST['statusOfEvent'];
-    $typeOfEvent = $_POST['typeOfEvent'];
-    $descriptionOfEvent = $_POST['descriptionOfEvent'];
+    $event = new Event();
 
-    $topicOfEvent = htmlspecialchars($topicOfEvent);
-    $descriptionOfEvent = htmlspecialchars($descriptionOfEvent);
+    $event->topicOfEvent = @$_POST['topicOfEvent']; echo 'topic of event set!';
+    if (!$event->topicOfEvent)
+        $error['topicOfEvent'] = 'Please insert the topic of this Event';
+
+    $event->idClient = $_POST['idClient']; echo 'Client of event set!';
+    if (!$event->idClient)
+        $error['idClient'] = 'Please indicate the Client';
+
+    $event->dateOfEvent = $_POST['dateOfEvent']; echo 'date of event set!';
+    if (!$event->dateOfEvent)
+        $error['dateOfEvent'] = 'Please insert the date';
+
+    $event->timeOfEvent = $_POST['timeOfEvent'];
+    if (!$event->timeOfEvent)
+        $error['timeOfEvent'] = 'Please insert the time';
+
+    $event->statusOfEvent = $_POST['statusOfEvent'];
+    if (!$event->statusOfEvent)
+        $error['statusOfEvent'] = 'Please indicate the status of this Event';
+
+    $event->typeOfEvent = $_POST['typeOfEvent'];
+    if (!$event->typeOfEvent)
+        $error['typeOfEvent'] = 'Please chose the type of Event';
+
+    $event->descriptionOfEvent = $_POST['descriptionOfEvent']; echo 'description of event set!';
+    if (!$event->descriptionOfEvent)
+        $error['descriptionOfEvent'] = 'Please insert the short description of this Event';
+
+    echo "<pre>";
+    print_r( $_POST);
+    echo "<br/><br/><br/>";
+    print_r( $event);
+    echo "</pre>";
 }
 
 // trigger for creation of new Event when all the required info in the form
 
-if ($topicOfEvent!="" &&
-    $idClient!="" &&
-    $dateOfEvent!="" &&
-    $timeOfEvent!="" &&
-    $statusOfEvent!="" &&
-    $typeOfEvent!="" &&
-    $descriptionOfEvent!="") {
-
-    $newEvent = new Event($topicOfEvent,
-                          $idClient,
-                          $dateOfEvent,
-                          $timeOfEvent,
-                          $statusOfEvent,
-                          $typeOfEvent,
-                          $descriptionOfEvent);
-    $newEvent->_sendEventDataToDB();
-    echo "<pre>";
-    print_r($newEvent);
-    echo "</pre><br/><br/>";
-
-} else {
-    echo 'Please insert the missing information <br/><br/>';
-}
+//if ($topicOfEvent!="" &&
+//    $idClient!="" &&
+//    $dateOfEvent!="" &&
+//    $timeOfEvent!="" &&
+//    $statusOfEvent!="" &&
+//    $typeOfEvent!="" &&
+//    $descriptionOfEvent!="") {
+//
+//    $newEvent = new Event($topicOfEvent,
+//                          $idClient,
+//                          $dateOfEvent,
+//                          $timeOfEvent,
+//                          $statusOfEvent,
+//                          $typeOfEvent,
+//                          $descriptionOfEvent);
+//    $newEvent->_sendEventDataToDB();
+//    echo "<pre>";
+//    print_r($newEvent);
+//    echo "</pre><br/><br/>";
+//
+//} else {
+//    echo 'Please insert the missing information <br/><br/>';
+//}
 
 ?>
 
@@ -154,40 +76,54 @@ ADD NEW EVENT:
 <form action="?" method="post">
     Client:
     <select name="idClient">
-        <option value="1" <?php if (@$idClient=='1') echo 'selected'; ?>>Coca-Cola</option>
-        <option value="2" <?php if (@$idClient=='2') echo 'selected'; ?>>Firma Bardzo Wazna i Fajna</option>
-        <option value="3" <?php if (@$idClient=='3') echo 'selected'; ?>>Spolka jakas bardzo tajna z o.o.</option>
-        <option value="4" <?php if (@$idClient=='4') echo 'selected'; ?>>Dell Inc.</option>
-        <option value="5" <?php if (@$idClient=='5') echo 'selected'; ?>>Default</option>
-        <option value="6" <?php if (@$idClient=='6') echo 'selected'; ?>>Olivia Business Center</option>
-        <option value="7" <?php if (@$idClient=='7') echo 'selected'; ?>>Szama mniam mniam</option>
-        <option value="8" <?php if (@$idClient=='8') echo 'selected'; ?>>Video Inc.</option>
-    </select><br/><br/>
+        <option value="1" <?php if (@$event->idClient=='1') echo 'selected'; ?>>Coca-Cola</option>
+        <option value="2" <?php if (@$event->idClient=='2') echo 'selected'; ?>>Firma Bardzo Wazna i Fajna</option>
+        <option value="3" <?php if (@$event->idClient=='3') echo 'selected'; ?>>Spolka jakas bardzo tajna z o.o.</option>
+        <option value="4" <?php if (@$event->idClient=='4') echo 'selected'; ?>>Dell Inc.</option>
+        <option value="5" <?php if (@$event->idClient=='5') echo 'selected'; ?>>Default</option>
+        <option value="6" <?php if (@$event->idClient=='6') echo 'selected'; ?>>Olivia Business Center</option>
+        <option value="7" <?php if (@$event->idClient=='7') echo 'selected'; ?>>Szama mniam mniam</option>
+        <option value="8" <?php if (@$event->idClient=='8') echo 'selected'; ?>>Video Inc.</option>
+    </select>
+    <div style="color: #23527c"><?php echo @$error['idClient'] ?></div>
+    <br/><br/>
 
     Topic:
-    <textarea name="topicOfEvent"><?php echo @$topicOfEvent ?></textarea><br/><br/>
+    <textarea name="topicOfEvent"><?php echo @$event->topicOfEvent ?></textarea>
+    <div style="color: #23527c"><?php echo @$error['topicOfEvent'] ?></div>
+    <br/><br/>
     Description:
-    <textarea name="descriptionOfEvent"><?php echo @$descriptionOfEvent ?></textarea><br/><br/>
+    <textarea name="descriptionOfEvent"><?php echo @$event->descriptionOfEvent ?></textarea>
+    <div style="color: #23527c"><?php echo @$error['descriptionOfEvent'] ?></div>
+    <br/><br/>
 
     Date:
-    <input type="date" name="dateOfEvent" value="<?php echo @$dateOfEvent ?>" /><br/><br/>
+    <input type="date" name="dateOfEvent" value="<?php echo @$event->dateOfEvent ?>" />
+    <div style="color: #23527c"><?php echo @$error['dateOfEvent'] ?></div>
+    <br/><br/>
     Time:
-    <input type="time" name="timeOfEvent" value="<?php echo @$timeOfEvent ?>" /><br/><br/>
+    <input type="time" name="timeOfEvent" value="<?php echo @$event->timeOfEvent ?>" />
+    <div style="color: #23527c"><?php echo @$error['timeOfEvent'] ?></div>
+    <br/><br/>
     Status:
     <select name="statusOfEvent">
-        <option value="01" <?php if (@$statusOfEvent=='01') echo 'selected'; ?>>Arranged</option>
-        <option value="02" <?php if (@$statusOfEvent=='02') echo 'selected'; ?>>Confirmed</option>
-        <option value="03" <?php if (@$statusOfEvent=='03') echo 'selected'; ?>>Completed</option>
-        <option value="04" <?php if (@$statusOfEvent=='04') echo 'selected'; ?>>Cancelled</option>
-    </select><br/><br/>
+        <option value="01" <?php if (@$event->statusOfEvent=='01') echo 'selected'; ?>>Arranged</option>
+        <option value="02" <?php if (@$event->statusOfEvent=='02') echo 'selected'; ?>>Confirmed</option>
+        <option value="03" <?php if (@$event->statusOfEvent=='03') echo 'selected'; ?>>Completed</option>
+        <option value="04" <?php if (@$event->statusOfEvent=='04') echo 'selected'; ?>>Cancelled</option>
+    </select>
+    <div style="color: #23527c"><?php echo @$error['statusOfEvent'] ?></div>
+    <br/><br/>
 
     Type of event:
     <select name="typeOfEvent">
-        <option value="01" <?php if (@$typeOfEvent=='01') echo 'selected'; ?>>Call</option>
-        <option value="02" <?php if (@$typeOfEvent=='02') echo 'selected'; ?>>Email</option>
-        <option value="03" <?php if (@$typeOfEvent=='03') echo 'selected'; ?>>Video conference</option>
-        <option value="04" <?php if (@$typeOfEvent=='04') echo 'selected'; ?>>Meeting</option>
-    </select><br/><br/>
+        <option value="01" <?php if (@$event->typeOfEvent=='01') echo 'selected'; ?>>Call</option>
+        <option value="02" <?php if (@$event->typeOfEvent=='02') echo 'selected'; ?>>Email</option>
+        <option value="03" <?php if (@$event->typeOfEvent=='03') echo 'selected'; ?>>Video conference</option>
+        <option value="04" <?php if (@$event->typeOfEvent=='04') echo 'selected'; ?>>Meeting</option>
+    </select>
+    <div style="color: #23527c"><?php echo @$error['typeOfEvent'] ?></div>
+    <br/><br/>
 
     <input type="submit" name="submitNewEvent" value="Submit" />
 </form>
