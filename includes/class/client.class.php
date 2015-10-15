83c3e8176f9c7a1de0af3d88cb1e8e4db456110b
katban <grancen@wp.pl>
2015-10-15 07:10:05 +0200

    crm-15-obiects: powrot do Matuszowego przykladu (1.0)

--- includes/class/client.class.php
+++ includes/class/client.class.php
@@ -19,14 +19,75 @@ class Client {
     protected $note;
     protected $date;
 
+//
+//    public function __construct($table=null) {
+//        if (isset ($table)) {
+//            foreach ($table as $key=>$value) {
+//                $this->$key = $value;
+//            }
+//        }
+//    }
 
-    public function __construct($table=null) {
-        if (isset ($table)) {
-            foreach ($table as $key=>$value) {
-                $this->$key = $value;
-            }
-        }
+    public function __set($parm_name, $parm_value) {
+        return $this->$parm_name = $parm_value;
     }
 
-
+    public function __get($parm_name) {
+        return $this->$parm_name;
+    }
 }
+
+
+$error = '';
+// przejecie danych z posta
+if (count($_POST)) {
+    $client = new Client();
+
+    if(!@$_POST['name'])
+        $error = 'Nazwa firmy jest wymagana';
+    else
+        $client->name = @$_POST['name'];
+    if(!@$_POST['phone'])
+        $error = 'Pole telefon jest wymagane';
+    else {
+        //walidacja prosta przeniesci pozniej do creatora
+        $client->phone = @$_POST['phone'];
+        if (!preg_match('/[0-9]{9,11}/', $client->phone))
+            $error = 'Telefon musi skladac sie z 9 do 11 cyfr';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>operacje na klietach</title>
+
+</head>
+<body>
+<?php
+if ($error) {
+    echo '<div style="color:#f00;">'.$error.'</div><br/>';
+}
+
+echo '<form action="?" method="post">';
+echo 'Nazwa: <input name="name" value="'.@$client->name.'"/><br/>';
+echo 'NIP: <input name="idTax" value="'.@$client->idTax.'" /><br/>';
+echo 'Adres[ulica;numer;kod]: <input name="address" value="" /><br/>';
+echo 'Miasto: <input name="city" value="" /><br/>';
+echo 'Telefon: <input name="phone" value="'.@$client->phone.'" /><br/>';
+echo 'fax: <input name="fax" value="" /><br/>';
+echo 'www: <input name="www" value="" /><br/>';
+echo 'e-mail: <input name="mail" value="" /><br/>';
+echo 'Note: <textarea name="note" value="" /></textarea>';
+echo '<button id="btn_send">ZAPISZ</button>';
+echo '</form>';
+
+?>
+
+
+
+</body>
+</html>
