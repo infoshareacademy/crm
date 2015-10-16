99ec60e01fd7021f0c78751a6405402f87f16c97
katban <grancen@wp.pl>
2015-10-16 11:36:08 +0200

    CRM-16 (before crm-15-obiects): move form from client.class to add-client.php

--- add-client.php
+++ add-client.php
@@ -1,4 +1,4 @@
-<?php include 'includes/header.php'; ?>
+<?php include 'includes/header.php';  include_once "includes/class/client.class.php"?>
 <div role="tabpanel" id="add-client">
 
     <section class="container-fluid">
@@ -9,13 +9,20 @@
         <article class="row">
             <div class="col-lg-12">
                 <h1> </h1>
-                <p>Integer odio nulla, feugiat at diam in, sodales consequat neque. Morbi tortor felis,
-                    egestas ut sodales sit amet, ultrices sit amet neque. Mauris rutrum condimentum
-                    fringilla. Donec commodo, enim ac cursus mattis, lorem tortor faucibus justo,
-                    feugiat placerat magna nulla ut nulla. Nam tempor ante vel mauris vestibulum
-                    ullamcorper. Vivamus nec ante consectetur, ultricies neque id, cursus justo.
-                    Pellentesque pharetra sollicitudin urna at tincidunt. Donec interdum magna id
-                    elit sodales blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+
+                <form action="?" method="post">
+                    Client name: <input name="name" value="<?php @$_POST['name']?>"/><br/><div style="color:#f00;"><?php @$error['name']?></div>
+                    Tax Id: <input name="idTax" value="<?php @$_POST['idTax']?>" /><br/><div style="color:#f00;"><?php @$error['idTax']?></div>
+                    Adres: <input name="address" value="<?php @$_POST['address']?>" /><br/>[street;number;post code]<div style="color:#f00;"><?php @$error['address']?></div>
+                    City: <input name="city" value="<?php @$_POST['city']?>" /><br/><div style="color:#f00;"><?php @$error['city']?></div>
+                    Phone: <input name="phone" value="<?php @$_POST['phone']?>" /><br/><div style="color:#f00;"><?php @$error['phone']?></div>
+                    fax: <input name="fax" value="<?php @@$_POST['fax']?>" /><br/><div style="color:#f00;"><?php @$error['fax']?></div>
+                    www: <input name="www" value="<?php @$_POST['www']?>" /><br/><div style="color:#f00;"><?php @$error['www']?></div>
+                    e-mail: <input name="mail" value="<?php @$_POST['mail']?>" /><br/><div style="color:#f00;"><?php @$error['mail']?></div>
+                    Note: <textarea name="note" /><?php @$_POST['note']?></textarea><div style="color:#f00;"><?php @$error['note']?></div>
+                    <button id="btn_send">ADD THIS</button>
+                    </form>
+
             </div>
         </article>
     </section>
