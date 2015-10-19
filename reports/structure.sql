f677946a59a842caee9639b7c3ff72dc6d247b51
fiona <anna.mikulinska@gmail.com>
2015-10-19 13:04:44 +0200

    structure file for Prod DB

--- reports/structure.sql
+++ reports/structure.sql
@@ -13,7 +13,39 @@ statusOfEvent ENUM ('missing status', '01', '02', '03', '04') NOT NULL,
 typeOfEvent ENUM ('missing type', '01', '02', '03', '04') NOT NULL,
 descriptionOfEvent VARCHAR(250) NULL,
 outcomeOfEvent ENUM ('missing outcome', '01', '02', '03'),
-creationOfEvent TIMESTAMP NOT NULL,
+creationOfEvent TIMESTAMP NOT NULL DEFAULT current_timestamp,
 CONSTRAINT fk_clients_events FOREIGN KEY (idClient) REFERENCES clients(idClient),
 CONSTRAINT fk_contacts_events FOREIGN KEY (idContact) REFERENCES contacts(idContact)
 ) DEFAULT CHARSET=utf8;
+
+DROP TABLE if EXISTS clients;
+
+CREATE TABLE clients (
+idClient INT NOT NULL  AUTO_INCREMENT PRIMARY KEY ,
+nameClient VARCHAR(80) NOT NULL ,
+idTax VARCHAR(10) NULL,
+addressClient TINYTEXT NULL ,
+cityClient VARCHAR(25) NOT NULL ,
+phoneClient INT NOT NULL ,
+faxClient INT NULL ,
+wwwClient VARCHAR(40) NULL ,
+mailClient VARCHAR(40) NOT NULL ,
+noteClient TEXT NULL ,
+creationDateClient TIMESTAMP NOT NULL DEFAULT current_timestamp
+);
+
+DROP TABLE if EXISTS contacts;
+
+CREATE TABLE contacts (
+idContact INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+idClient INT NOT NULL,
+surnameContact VARCHAR(30) NOT NULL,
+nameContact VARCHAR(30) NULL,
+positionContact VARCHAR(30) NULL,
+phoneContact VARCHAR(15) NULL,
+emailContact VARCHAR(50) NOT NULL,
+cityContact VARCHAR(30) NOT NULL,
+linkedInContact VARCHAR(200) NULL,
+noteContact TINYTEXT NULL,
+creationDateContact TIMESTAMP NOT NULL DEFAULT current_timestamp
+)
