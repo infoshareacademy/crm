175a90b9f81811a073bad01d1015e9f0f1fb1eee
fiona <anna.mikulinska@gmail.com>
2015-10-27 16:41:17 +0100

    complete DAO class for single report

--- includes/classes/ClientEventsReportDAO.php
+++ includes/classes/ClientEventsReportDAO.php
@@ -1,26 +1,22 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: paoolskoolsky
- * Date: 21.10.15
- * Time: 16:12
- */
+
 class ClientEventsReportDAO
 {
-
-    public function getReportData() {
-        $query = "SELECT e.idClient, nameClient as Client,
-        count(*) AS 'events per month',
-        month(dateOfEvent) AS 'month',
-        year(dateOfEvent) AS 'year'
+    public function getReportData($idClient) {
+        $query = "SELECT e.idClient, nameClient,
+        count(*) AS countByMonth,
+        month(dateOfEvent) AS month,
+        year(dateOfEvent) AS year
         FROM events e INNER JOIN clients c on c.idClient = e.idClient
-        WHERE e.idClient = 1 AND dateOfEvent <> 0
-        GROUP BY Client, year(dateOfEvent), month(dateOfEvent)
+        WHERE e.idClient = 1 AND dateOfEvent <> 0 AND dateOfEvent < NOW()
+        GROUP BY nameClient, year(dateOfEvent), month(dateOfEvent)
         ORDER BY year(dateOfEvent), month(dateOfEvent)";
 
+        $input_parameters = array(':idClient' => $idClient);
+
         $stmt = DBConnection::getConnection()->prepare($query);
-        $input_parameters = array(':idClient' => $this->idClient);
+
         $status = $stmt->execute($input_parameters);
 
         if ($status>0) {
@@ -28,8 +24,7 @@ class ClientEventsReportDAO
         } else {
             echo 'Oups! Are you sure yu have ever actually met this Client?';
         }
-
+        return $reportData;
     }
-
 
 }
