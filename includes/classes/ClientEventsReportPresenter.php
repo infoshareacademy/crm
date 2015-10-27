a251b3da4949a75ffdf7c5255463d136f3879547
fiona <anna.mikulinska@gmail.com>
2015-10-27 17:04:03 +0100

    sketch of Presenter for single report

---
+++ includes/classes/ClientEventsReportPresenter.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class ClientEventsReportPresenter
+{
+
+    public function display($idClient) {
+        $dao = new ClientEventsReportDAO();
+        $data = $dao->getReportData($idClient);
+        $reports = [];
+        foreach($data as $oneReportData) {
+            $report = new ClientEventsReport();
+            $report->fillData($oneReportData);
+            $reports[] = $report;
+        }
+        return drawReport($reports);
+    }
+
+    protected function drawReport(/* [ClientEventsReport] */$reports) {
+        return '<div>';
+    }
+
+}
