ad38e94aa92f15979ff55622015efd2771433b10
fiona <anna.mikulinska@gmail.com>
2015-10-27 17:03:30 +0100

    first version of the Container Class for single report

--- includes/classes/ClientEventsReport.php
+++ includes/classes/ClientEventsReport.php
@@ -1,26 +1,22 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: paoolskoolsky
- * Date: 21.10.15
- * Time: 15:21
- */
-
 require_once __DIR__ . '/DBConnection.php';
 
 class ClientEventsReport
 {
-    public function getRaportData()
-    {
-        $pdo = DBConnection::getConnection();
-        $stmt = $pdo->query("Select e.idClient, nameClient as Client, count(*) AS 'events per month', month(dateOfEvent) AS month,
-                   year(dateOfEvent) AS year
-FROM events e
-  INNER JOIN clients c
-    on c.idClient = e.idClient
-WHERE e.idClient = 1 and dateOfEvent < NOW() + INTERVAL 1 year and dateOfEvent <> 0
-GROUP BY month(dateOfEvent) ORDER BY year(dateOfEvent) DESC, month(dateOfEvent) DESC");
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    protected $idClient;
+    protected $nameClient;
+    protected $countByMonth;
+    protected $month;
+    protected $year;
+
+    public function fillData($oneReportData) {
+        foreach ($oneReportData as $item){
+        $this->idClient = $item['idClient'];
+        $this->nameClient = $item['nameClient'];
+        $this->countByMonth = $item['countByMonth'];
+        $this->month = $item['month'];
+        $this->year = $item['year'];
+        }
     }
 }
