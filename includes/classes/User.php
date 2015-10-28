6e3b061a9b0aff05337d3e8c7174cc8425054bc0
katban <grancen@wp.pl>
2015-10-28 19:11:00 +0100

    CRM-8: more tests :P

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/UserDAO.php';
+
 /**
  * Created by PhpStorm.
  * User: katban
@@ -19,52 +20,56 @@ class User
     const USER_USER = 1;
     const USER_GUEST = 0;
 
-    public function __construct($login = null, $pass = null, $permissions = null, $forTest=false) {
-        if($forTest == true) {
+    public function __construct($forTest = false)
+    {
+        if ($forTest == true) {
             return;
         }
 
         session_start();
-        if(isset($_SESSION['user'])) {
+        if (isset($_SESSION['user'])) {
             $this->login = $_SESSION['user'];
             $this->permissions = $_SESSION['permissions'];
             $this->logged = true;
-        }
-        else {
+        } else {
             $this->logged = false;
         }
     }
 
-    public function __get($param_name) {
+    public function __get($param_name)
+    {
         return $this->$param_name;
     }
 
-    public function __set($param_name, $param_value) {
+    public function __set($param_name, $param_value)
+    {
         $this->$param_name = $param_value;
     }
 
-    public static function getUser() {
-        if(!User::$aliveUser) {
+    public static function getUser()
+    {
+        if (!User::$aliveUser) {
             User::$aliveUser = new User();
         }
         return User::$aliveUser;
     }
 
-    public function setPassword($pass) {
+    public function setPassword($pass)
+    {
         $pass = md5($pass);
         $this->pass = $pass;
     }
 
-    public function login($userName) {
-        $dbUser = new userDAO();
+    public function login($userName, userDAO $dbUser = null)
+    {
+        $dbUser = $dbUser ?: new userDAO();
         $permissions = $dbUser->autorization($this);
 
         if ($permissions != null) {
             //autoryzacja sie powiodla i zostal zwrocony poziom uprawnien
             $this->logged = true;
             $this->permissions = $permissions;
-        }
-        else {
+        } else {
             $this->logged = false;
         }
         $this->pass = null;
--- includes/tests/UserTest.php
+++ includes/tests/UserTest.php
@@ -13,7 +13,7 @@ class userTest extends PHPUnit_Framework_TestCase
     public function testPasswordEncoding()
     {
         // given
-        $instance = new User(null,null,null,true);
+        $instance = new User(true);
         // when
         $password = 'brzydkieHaslo';
         $instance->setPassword($password);
@@ -21,4 +21,47 @@ class userTest extends PHPUnit_Framework_TestCase
         $this->assertNotEquals($instance->pass, $password);
 
     }
+
+    public function testLoginCorrectUser()
+    {
+        //given
+        $permission = 2;
+        $instance = new User(true);
+        $DAO = $this->getMock('UserDAO');
+        $DAO->expects($this->any())->method('autorization')->will($this->returnValue($permission));
+
+        //when
+        $instance->login('testUserName', $DAO);
+        //then
+        $this->assertEquals($instance->logged,true);
+        $this->assertEquals($instance->permissions, $permission);
+    }
+
+    public function testClearPasswordAfterSuccessLogin()
+    {
+        //given
+        $instance = new User(true);
+        $instance->setPassword('zzzz');
+        $DAO = $this->getMock('UserDAO');
+        $DAO->expects($this->any())->method('autorization')->will($this->returnValue(2));
+        //when
+        $instance->login('aaa', $DAO);
+        //then
+        $this->assertNull($instance->pass);
+    }
+
+    public function testClearPassworAfterFailedLogin()
+    {
+        $instance = new User(true);
+        $instance->setPassword('zzzz');
+        $DAO = $this->getMock('UserDAO');
+        $DAO->expects($this->any())->method('autorization')->will($this->returnValue(null));
+
+        $instance->login('bbbb', $DAO);
+
+        $this->assertNull($instance->pass);
+
+    }
+
+
 }
