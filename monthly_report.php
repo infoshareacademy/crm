8f6f4a1ac3da83783bc6f0c6be62d9e0c680869e
fiona <anna.mikulinska@gmail.com>
2015-10-30 16:56:05 +0100

    working dropdown with list of Clients from DB

---
+++ monthly_report.php
@@ -0,0 +1,41 @@
+<?php
+
+include 'includes/header.php';
+require_once __DIR__ . '/includes/classes/ClientEventsReportPresenter.php';
+require_once __DIR__ . '/includes/classes/Event.php';
+?>
+    <div role="tabpanel" id="generate-report" xmlns="http://www.w3.org/1999/html">
+
+        <section class="container-fluid">
+            <figure class="banner">
+                <figcaption>Generate report</figcaption>
+            </figure>
+            <div>
+                In order to generate a monthly report please choose the Client from the list:</br>
+                <form action="?" method="get">
+                    <select name="idClient">
+                    <?php
+                $listOfClients = Event::displayFromEvents('Client');
+                    foreach ($listOfClients as $item) {
+                    echo "<option value='" .
+                        $item['idClient'].
+                        "'>" .
+                        $item['nameClient'] .
+                        "</option>";
+                }
+                ?>
+                    </select>
+                    <input type="submit" name="submitNewEvent" value="Generate" />
+                    </form>
+            </div>
+
+            <?php
+            $displayer = new ClientEventsReportPresenter();
+            echo $displayer->display(2);
+            ?>
+        </section>
+    </div>
+
+<?php
+include 'includes/footer.php';
+?>
