245dda95614818bde7e878659d488716577a4f2a
katban <grancen@wp.pl>
2015-10-30 15:36:54 +0100

    CRM-50: Change language (PL -> EN)

--- add-client.php
+++ add-client.php
@@ -13,24 +13,24 @@ if (count($_POST)) {
     //pola wymagane
     $client->name = @$_POST['name'];
     if(!$client->name)
-        $error['name'] = 'Podaj nazwe kontrahenta';
+        $error['name'] = 'Name is required';
 
     $client->city = @$_POST['city'];
     if(!$client->city)
-        $error['city'] = 'Wpisz miasto';
+        $error['city'] = 'City is required';
 
     $client->phone = (int)@$_POST['phone'];
     if(!$client->phone)
-        $error['phone'] = 'Numer musi skladac sie z 9-11  cyfr';
+        $error['phone'] = 'Phone is required (9-11 numbers)';
 
     $client->mail = @$_POST['mail'];
     if(!$client->mail)
-        $error['mail'] = 'Podaj prawidlowy mail z @ i poprawna domena';
+        $error['mail'] = 'Mail address is required (ex. mail@domain.com)';
 
     // nieobowiazkowe
     $client->idTax = @$_POST['idTax'];
     if($client->idTax === $blad)
-        $error['idTax'] = 'Tax ID moze miec max 11 cyfr';
+        $error['idTax'] = 'Max lenght 11 numbers';
 
     // zebranie danych z trzech pol formularza i spreparowanie ich tak jak sa przechowywane w db
     // street ; streetNumber ; postcode
@@ -44,29 +44,29 @@ if (count($_POST)) {
     }
 
     if($client->address === $blad)
-        $error['address'] = 'Max 255 znakow';
+        $error['address'] = 'Max length 255';
 
     $client->fax = @$_POST['fax'];
     if($client->fax === $blad)
-        $error['fax'] = 'Numer musi skladac sie z 9-11  cyfr';
+        $error['fax'] = 'Required 9-11 numbers';
 
     $client->www = @$_POST['www'];
     if($client->www === $blad)
-        $error['www'] = 'Domena do 40 znakow';
+        $error['www'] = 'Max lenght 40 characters';
 
     $client->note = @$_POST['note'];
     if($client->note === $blad)
-        $error['note'] = 'to ma byc krotka notatka do 100 znakow ;)';
+        $error['note'] = 'Max length 100 characters';
 
     //zapis
     if(count($error) == 0) {
-        //echo 'Proba zapisu<br/><br/><br/>';
         $status = $client->save();
         if($status == Client::SAVE_STATUS_OK) {
-            $success = "Qrde udalo sie";
+            $success = "Great! It's save!";
+            header("Location: clients-list.php");
         }
         else {
-            $error['global'] = 'Fatalnie nie da rady zapisac tego Clienta';
+            $error['global'] = 'Fatally! I can\' save this Client!';
         }
     }
 
@@ -87,22 +87,22 @@ if (count($_POST)) {
 
                 <?php
                     if (@$success)
-                    echo '<div style="color:#22aa22; font-weight:bold;">'.$success.'</div><br/>';
+                    echo '<div class="text-uppercase text-success">'.$success.'</div><br/>';
 
                     if (@$error['general'])
-                    echo '<div style="color:#f00; font-weight:bold;">'.$error['general'].'</div><br/>';
+                    echo '<div class="text-uppercase text-danger">'.$error['general'].'</div><br/>';
                 ?>
 
                 <form action="?" method="post">
-                    Client name: <input name="name" value='<?php echo @$_POST["name"]?>'><br/><div style="color:#f00;"><?php echo @$error['name']?></div>
-                    Tax Id: <input name="idTax" value="<?php echo @$_POST['idTax']?>" /><br/><div style="color:#f00;"><?php echo @$error['idTax']?></div>
-                    Street: <input name="street" value="<?php echo @$_POST['street']?>" /> No: <input name="streetNumber" size="6" value="<?php echo @$_POST['streetNumber']?>" />Post Code: <input name="postCode" size="10" value="<?php echo @$_POST['postCode']?>" /><br/><div style="color:#f00;"><?php echo @$error['address']?></div>
-                    City: <input name="city" value="<?php echo @$_POST['city']?>" /><br/><div style="color:#f00;"><?php echo @$error['city']?></div>
-                    Phone: <input name="phone" value="<?php echo @$_POST['phone']?>" /><br/><div style="color:#f00;"><?php echo @$error['phone']?></div>
-                    fax: <input name="fax" value="<?php echo @$_POST['fax']?>" /><br/><div style="color:#f00;"><?php echo @$error['fax']?></div>
-                    www: <input name="www" value="<?php echo @$_POST['www']?>" /><br/><div style="color:#f00;"><?php echo @$error['www']?></div>
-                    e-mail: <input name="mail" value="<?php echo @$_POST['mail']?>" /><br/><div style="color:#f00;"><?php echo @$error['mail']?></div>
-                    Note: <textarea name="note" /><?php echo @$_POST['note']?></textarea><div style="color:#f00;"><?php echo @$error['note']?></div>
+                    Client name *: <input name="name" value='<?php echo @$_POST["name"]?>'><br/><div class='small text-uppercase text-danger'><?php echo @$error['name']?></div>
+                    Tax Id: <input name="idTax" value="<?php echo @$_POST['idTax']?>" /><br/><div class='small text-uppercase text-danger'><?php echo @$error['idTax']?></div>
+                    Street: <input name="street" value="<?php echo @$_POST['street']?>" /> No: <input name="streetNumber" size="6" value="<?php echo @$_POST['streetNumber']?>" /> Post Code: <input name="postCode" size="10" value="<?php echo @$_POST['postCode']?>" /><br/><div class='small text-uppercase text-danger'><?php echo @$error['address']?></div>
+                    City *: <input name="city" value="<?php echo @$_POST['city']?>" /><br/><div class='small text-uppercase text-danger'><?php echo @$error['city']?></div>
+                    Phone *: <input name="phone" value="<?php echo @$_POST['phone']?>" /><br/><div class='small text-uppercase text-danger'><?php echo @$error['phone']?></div>
+                    fax: <input name="fax" value="<?php echo @$_POST['fax']?>" /><br/><div class='small text-uppercase text-danger'><?php echo @$error['fax']?></div>
+                    www: <input name="www" value="<?php echo @$_POST['www']?>" /><br/><div class='small text-uppercase text-danger'><?php echo @$error['www']?></div>
+                    e-mail *: <input name="mail" value="<?php echo @$_POST['mail']?>" /><br/><div class='small text-uppercase text-danger'><?php echo @$error['mail']?></div>
+                    Note: <textarea name="note" /><?php echo @$_POST['note']?></textarea><div class='small text-uppercase text-danger'><?php echo @$error['note']?></div>
                     <button id="btn_send">ADD THIS</button>
                 </form>
 
